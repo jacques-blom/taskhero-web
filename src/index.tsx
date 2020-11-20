c8fc3334656e87a7ac5a2390f2cdefd23a0f1bf0
Jacques Blom <39128232+jacquesnf@users.noreply.github.com>
2020-11-20 14:29:11 +0200

    Remove MSW and associated tests

--- src/index.tsx
+++ src/index.tsx
@@ -8,11 +8,6 @@ import {DarkModeProvider} from './components/useDarkMode'
 import {UserIdProvider} from './components/useUserId'
 import 'react-toastify/dist/ReactToastify.css'
 
-// if (process.env.NODE_ENV === 'development') {
-//     const {worker} = require('./mocks/browser')
-//     worker.start()
-// }
-
 ReactDOM.render(
     <React.StrictMode>
         <DarkModeProvider>
