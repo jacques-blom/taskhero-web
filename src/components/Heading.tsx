dcba10dd586bad9313136ea5e021789d147318e7
Jacques Blom <jacquesblom@Jacquess-MacBook-Pro.local>
2020-11-29 15:36:32 +0200

    Use emoji logo

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -2,19 +2,23 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
+    display: flex;
+    align-items: center;
     font-family: 'Playfair Display', serif;
     font-size: 47px;
     font-weight: 600;
     letter-spacing: 2px;
     color: ${(props) => props.theme.text};
+
+    > img {
+        margin-right: 10px;
+    }
 `
 
 export const Heading: React.FC = () => {
     return (
         <Container>
-            <span role="img" aria-label="Taskhero logo">
-                🦸‍♂️
-            </span>{' '}
+            <img src="/logo.png" style={{height: 47}} alt="Taskhero logo" />
             Taskhero
         </Container>
     )
