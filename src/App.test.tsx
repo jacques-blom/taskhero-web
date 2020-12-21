bfa69a555e4c6cc02e51a64e5f37cc1f9920bd75
Jacques Blom <39128232+jacques-blom@users.noreply.github.com>
2020-12-21 12:44:54 +0200

    Tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import {render, screen, waitForElementToBeRemoved} from '@testing-library/react'
+import {fireEvent, render, screen, waitFor, waitForElementToBeRemoved} from '@testing-library/react'
 import App from './App'
 import {GlobalWrapper} from './testUtils'
 import {server} from './mocks/server'
 import {rest} from 'msw'
 import {getApiUrl} from './components/api'
+import userEvent from '@testing-library/user-event'
+import {singleTask} from './mocks/handlers'
+
+const waitForLoading = () => {
+    return waitForElementToBeRemoved(() => screen.getByRole('alert', {name: 'loading'}))
+}
 
 it('shows the loading spinner while data is loading', () => {
     render(<App />, {wrapper: GlobalWrapper})
@@ -13,7 +19,7 @@ it('shows the loading spinner while data is loading', () => {
 
 it('shows an alert if there are no tasks', async () => {
     render(<App />, {wrapper: GlobalWrapper})
-    await waitForElementToBeRemoved(() => screen.getByRole('alert', {name: 'loading'}))
+    await waitForLoading()
     expect(screen.getByRole('heading', {name: /No tasks yet/i})).toBeInTheDocument()
 })
 
@@ -25,7 +31,93 @@ it('shows an error message if the API returns an error', async () => {
     )
 
     render(<App />, {wrapper: GlobalWrapper})
-    await waitForElementToBeRemoved(() => screen.getByRole('alert', {name: 'loading'}))
+    await waitForLoading()
     expect(screen.getByRole('heading', {name: /error/i})).toBeInTheDocument()
     expect(screen.getByRole('alert', {name: /internal server error/i})).toBeInTheDocument()
+})
+
+it('inserts a new task', async () => {
+    render(<App />, {wrapper: GlobalWrapper})
+    await waitForLoading()
+
+    const insertInput = screen.getByRole('textbox', {name: /insert/i})
+
+    // Type a task and press enter
+    userEvent.type(insertInput, 'New task')
+    fireEvent.keyUp(insertInput, {keyCode: 13})
+
+    // Test the loading state
+    expect(insertInput).toBeDisabled()
+
+    // Test the success state
+    await waitFor(() => expect(insertInput).not.toBeDisabled())
+    expect(insertInput).toHaveValue('')
+
+    // Test whether the task is displaying on the page
+    expect(screen.getByTestId(/task-/)).toHaveTextContent('New task')
+})
+
+it('displays an error message if the API fails', async () => {
+    render(<App />, {wrapper: GlobalWrapper})
+    await waitForLoading()
+
+    const insertInput = screen.getByRole('textbox', {name: /insert/i})
+
+    // Just press enter without typing a label
+    fireEvent.keyUp(insertInput, {keyCode: 13})
+
+    // Wait for loading to complete
+    await waitFor(() => expect(insertInput).not.toBeDisabled())
+
+    // Expect an error alert to display
+    expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(`"Missing label"`)
+})
+
+it('toggles the task completed state', async () => {
+    // Mock a single task on the page
+    server.use(singleTask)
+
+    render(<App />, {wrapper: GlobalWrapper})
+    await waitForLoading()
+
+    // Click the checkbox
+    userEvent.click(screen.getByRole('checkbox', {name: /mark as completed/}))
+
+    // Expect it to be disabled while loading
+    expect(screen.getByRole('checkbox')).toBeDisabled()
+
+    // Wait for the checkbox to be checked
+    await waitFor(() => expect(screen.getByRole('checkbox')).toBeChecked())
+
+    // Click the now-checked checkbox
+    userEvent.click(screen.getByRole('checkbox', {name: /mark as uncompleted/}))
+
+    // Wait for the checkbox to be unchecked
+    await waitFor(() => expect(screen.getByRole('checkbox')).not.toBeChecked())
+})
+
+it('handles toggling the completed state failing', async () => {
+    // Display a single task again
+    server.use(singleTask)
+
+    // Return an error response from the API when we try to call this endpoint
+    server.use(
+        rest.post(getApiUrl('/task/:id'), (req, res, ctx) =>
+            res(ctx.status(500), ctx.json({message: 'Something went wrong'})),
+        ),
+    )
+
+    render(<App />, {wrapper: GlobalWrapper})
+    await waitForLoading()
+
+    // Click the checkbox
+    userEvent.click(screen.getByRole('checkbox', {name: /mark as completed/}))
+
+    // Expect the error to display once loading has completed
+    await waitFor(() => {
+        return expect(screen.getByRole('alert').textContent).toMatchInlineSnapshot(`"Something went wrong"`)
+    })
+
+    // Make sure the checkbox stays unchecked
+    expect(screen.getByRole('checkbox')).not.toBeChecked()
 })
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,8 +1,63 @@
 import {rest} from 'msw'
+import {v4} from 'uuid'
 import {getApiUrl} from '../components/api'
+
+export const singleTask = rest.get(getApiUrl('/tasks'), (req, res, ctx) => {
+    return res(
+        ctx.json([
+            {
+                id: v4(),
+                label: 'Example',
+                completed: false,
+            },
+        ]),
+    )
+})
 
 export const handlers = [
     rest.get(getApiUrl('/tasks'), (req, res, ctx) => {
         return res(ctx.json([]))
+    }),
+
+    rest.post(getApiUrl('/tasks'), (req, res, ctx) => {
+        // Make sure we receive a request body as a string
+        if (typeof req.body !== 'string') throw new Error('Missing request body')
+
+        // Parse the request body
+        const newTask = JSON.parse(req.body)
+
+        // Emulate our real API's behaviour by throwing if we don't receive a label
+        if (newTask.label.length === 0) {
+            return res(ctx.status(400), ctx.json({message: 'Missing label'}))
+        }
+
+        // Emulate our real API's behaviour by responding with the new full task object
+        return res(
+            ctx.json({
+                id: v4(),
+                label: newTask.label,
+                completed: false,
+            }),
+        )
+    }),
+
+    rest.post(getApiUrl('/task/:id'), (req, res, ctx) => {
+        // Make sure we receive a request body as a string
+        if (typeof req.body !== 'string') throw new Error('Missing request body')
+
+        // Parse the request body
+        const newTask = JSON.parse(req.body)
+
+        // Get the task ID from the route parameter
+        const taskId = req.params.id
+
+        // Emulate our real API's behaviour by responding with the updated task object
+        return res(
+            ctx.json({
+                id: taskId,
+                label: 'Example',
+                completed: newTask.completed,
+            }),
+        )
     }),
 ]
